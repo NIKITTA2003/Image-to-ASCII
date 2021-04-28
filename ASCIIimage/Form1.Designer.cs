1b6af016f5aef418ac45cd651d4309faf6539122
NIKITTA2003 <51858919+NIKITTA2003@users.noreply.github.com>
2021-04-28 23:04:58 +0500

    Add files via upload

---
+++ ASCIIimage/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ASCIIimage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 5.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1280, 720);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt file|*.txt";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "Выберите папку";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Воспроизвести";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1252, 720);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Font = new System.Drawing.Font("Consolas", 2.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fontDialog1.ShowColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(403, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Video2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Имя файла для сохранения";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(734, 12);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(146, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(628, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Размер шрифта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Tan;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ASCIIimage/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+
+namespace ASCIIimage
+{
+    public partial class Form1 : Form
+    {
+        Thread[] p = new Thread[2];
+        public static string str1 = "";
+        public static string str2 = "";
+        public static string filePath;
+        public static bool vk = false;
+
+        StreamReader read;
+        string result;
+        string[] frames;
+        int frame = 0;
+        Font f;
+        Graphics g;
+
+        Bitmap map;
+
+        Pen pen = new Pen(Color.White);
+        SolidBrush br;
+        int lenght;
+        public Form1()
+        {
+            InitializeComponent();
+            map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            g = Graphics.FromImage(map);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            g.Clear(Color.Black);
+
+            br = new SolidBrush(Color.Green);
+            f = fontDialog1.Font;
+            pictureBox1.Image = map;
+        }
+
+        public static void ConvertImage()
+        {
+            Image img = Image.FromFile(filePath);
+            Bitmap bmp = new Bitmap(img, new Size(img.Width, img.Height));
+            img.Dispose();
+            Rectangle bounds = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            ColorMatrix matrix = new ColorMatrix();
+
+            for (int i = 0; i <= 2; i++)
+                for (int j = 0; j <= 2; j++)
+                    matrix[i, j] = 1 / 3f;
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(matrix);
+
+            Graphics gphGrey = Graphics.FromImage(bmp);
+            gphGrey.DrawImage(bmp, bounds, 0, 0, bmp.Width, bmp.Height, GraphicsUnit.Pixel, attributes);
+
+            gphGrey.Dispose();
+
+            int startY, startX, brightness, cY, cX, b, sb;
+            Color c;
+
+            for (int h = 0; h < bmp.Height/2; h++)
+            {
+                 startY = h;
+
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                     startX = w;
+
+                     brightness = 0;
+
+                    for (int y = 0; y < 10; y++)
+                    {
+                        for (int x = 0; x < 10; x++)
+                        {
+                             cY = y + startY;
+                             cX = x + startX;
+
+                            try
+                            {
+                                 c = bmp.GetPixel(cX, cY);
+                                 b = (int)(c.GetBrightness() * 10);
+                                brightness = (brightness + b);
+                            }
+                            catch
+                            {
+                                brightness = (brightness + 10);
+                            }
+                        }
+                    }
+
+                     sb = (brightness / 10);
+                    if (vk)
+                    {
+                        if (sb > 50)
+                        {
+                            str1 = str1 + 'Ж';
+                        }
+                        else
+                            str1 = str1 + ' ';
+                    }
+                    else
+                    {
+                        if (sb > 80)
+                        {
+                            str1 = str1 + '#';
+                        }
+                        else if (sb > 75)
+                        {
+                            str1 = str1 + '@';
+                        }
+                        else if (sb > 70)
+                        {
+                            str1 = str1 + 'Ш';
+                        }
+                        else if (sb > 65)
+                        {
+                            str1 = str1 + '$';
+                        }
+                        else if (sb > 60)
+                        {
+                            str1 = str1 + '&';
+                        }
+                        else if (sb > 55)
+                        {
+                            str1 = str1 + 'Ё';
+                        }
+                        else if (sb > 50)
+                        {
+                            str1 = str1 + 'Ё';
+                        }
+                        else if (sb > 45)
+                        {
+                            str1 = str1 + 'г';
+                        }
+                        else if (sb > 40)
+                        {
+                            str1 = str1 + '¤';
+                        }
+                        else if (sb > 35)
+                        {
+                            str1 = str1 + '/';
+                        }
+                        else if (sb > 30)
+                        {
+                            str1 = str1 + '^';
+                        }
+                        else if (sb > 25)
+                        {
+                            str1 = str1 + '~';
+                        }
+                        else if (sb > 10)
+                        {
+                            str1 = str1 + '.';
+                        }
+                        else
+                        {
+                            str1 = str1 + " ";
+                        }
+                    }
+
+                }
+                    str1 = str1 + '\n';
+
+            }
+            bmp.Dispose();
+        }
+
+        public static void ConvertImage2()
+        {
+            Image img = Image.FromFile(filePath);
+            Bitmap bmp = new Bitmap(img, new Size(img.Width, img.Height));
+            img.Dispose();
+            Rectangle bounds = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            ColorMatrix matrix = new ColorMatrix();
+
+            for (int i = 0; i <= 2; i++)
+                for (int j = 0; j <= 2; j++)
+                    matrix[i, j] = 1 / 3f;
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(matrix);
+
+            Graphics gphGrey = Graphics.FromImage(bmp);
+            gphGrey.DrawImage(bmp, bounds, 0, 0, bmp.Width, bmp.Height, GraphicsUnit.Pixel, attributes);
+
+            gphGrey.Dispose();
+
+            int startY, startX, brightness, cY, cX, b, sb;
+            Color c;
+
+            for (int h = (bmp.Height / 2); h < bmp.Height; h++)
+            {
+                 startY = h;
+
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                     startX = w ;
+
+                     brightness = 0;
+
+                    for (int y = 0; y < 10; y++)
+                    {
+                        for (int x = 0; x < 10; x++)
+                        {
+                             cY = y + startY;
+                             cX = x + startX;
+
+                            try
+                            {
+                                 c = bmp.GetPixel(cX, cY);
+                                 b = (int)(c.GetBrightness() * 10);
+                                brightness = (brightness + b);
+                            }
+                            catch
+                            {
+                                brightness = (brightness + 10);
+                            }
+                        }
+                    }
+
+                     sb = (brightness / 10);
+                    if (vk)
+                    {
+                        if (sb > 50)
+                        {
+                            str2 = str2 + 'Ж';
+                        }
+                        else
+                            str2 = str2 + ' ';
+                    }
+                    else
+                    {
+                        if (sb > 80)
+                        {
+                            str2 = str2 + '#';
+                        }
+                        else if (sb > 75)
+                        {
+                            str2 = str2 + '@';
+                        }
+                        else if (sb > 70)
+                        {
+                            str2 = str2 + 'Ш';
+                        }
+                        else if (sb > 65)
+                        {
+                            str2 = str2 + '$';
+                        }
+                        else if (sb > 60)
+                        {
+                            str2 = str2 + '&';
+                        }
+                        else if (sb > 55)
+                        {
+                            str2 = str2 + 'Ё';
+                        }
+                        else if (sb > 50)
+                        {
+                            str2 = str2 + 'Ё';
+                        }
+                        else if (sb > 45)
+                        {
+                            str2 = str2 + 'г';
+                        }
+                        else if (sb > 40)
+                        {
+                            str2 = str2 + '¤';
+                        }
+                        else if (sb > 35)
+                        {
+                            str2 = str2 + '/';
+                        }
+                        else if (sb > 30)
+                        {
+                            str2 = str2 + '^';
+                        }
+                        else if (sb > 25)
+                        {
+                            str2 = str2 + '~';
+                        }
+                        else if (sb > 10)
+                        {
+                            str2 = str2 + '.';
+                        }
+                        else
+                        {
+                            str2 = str2 + " ";
+                        }
+                    }
+
+                }
+                str2 = str2 + '\n';
+
+            }
+            bmp.Dispose();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                string[] filesname = new string[1];
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filesname = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+
+
+
+                    StreamWriter SW = new StreamWriter(new FileStream(textBox1.Text + ".txt", FileMode.Create, FileAccess.Write));
+
+                    for (int i = 0; i <= filesname.Length; i++)
+                    {
+
+                        filePath = filesname[i];
+
+                        p[0] = new Thread(ConvertImage);
+                        p[1] = new Thread(ConvertImage2);
+                        p[0].Priority = ThreadPriority.Highest;
+                        p[0].Start();
+                        p[1].Start();
+
+                        while (!(p[0].IsAlive == false && p[1].IsAlive == false))
+                        { }
+                        SW.Write(str1 + str2 + "ъ" + "\n");
+                        str1 = "";
+                        str2 = "";
+                    }
+                    MessageBox.Show("Готово :)");
+                    SW.Close();
+                    button2.Enabled = true;
+                }
+            }
+            else
+                MessageBox.Show("Задайте имя файла для сохранения");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Width = 1280 ;
+            richTextBox1.Height = 720 ;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                read = new StreamReader(openFileDialog1.FileName);
+                result = read.ReadToEnd();
+                frames = result.Split('ъ');
+                timer1.Enabled = true;
+                lenght = frames.Length;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (frame < lenght)
+            {
+                g.Clear(Color.Black);
+
+                g.DrawString(frames[frame].Trim(), f, br, 0, 0);
+                pictureBox1.Image = map;
+                frame++;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                frame = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("Matrix.jpg");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            f = new Font(f.Name, trackBar1.Value, f.Style);
+        }
+    }
+}
